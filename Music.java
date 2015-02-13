f527e335ec954d27edc4f681104594755f4f53ff
Brian Capouch <brianc@saintjoe.edu>
2015-02-13 15:39:20 -0500

    Initial files

---
+++ Music.java
@@ -0,0 +1,30 @@
+public class Music {
+  
+  // Class to keep track of music
+  // CMP112 Brian Capouch 13 February 2015
+
+  public String album;
+  public String artist;
+  public String genre;
+  public String media;
+  public String location;
+  public int copies;
+
+/* ----------------------------------------------------------------------- */
+
+  Music() {
+    album = "No album";
+    artist = "No arist";
+    genre = "No genre";
+    media = "None";
+    location = "Nowhere";
+    copies = 0;
+    }
+
+  public String toString() {
+    String output = "";
+    output = "Album: " + album + " Artist: " + artist + " Genre: " + genre + " Media: " + media + " Location: " + location + " Copies: " + copies;
+    return output;
+    }
+
+}
