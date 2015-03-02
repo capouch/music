7c31c5670de1a015f98cf4a03e5e292ee7bac165
Brian Capouch <brianc@saintjoe.edu>
2015-03-02 03:07:27 -0500

    Test changes

--- MusicDriver.java
+++ MusicDriver.java
@@ -4,5 +4,6 @@ public class MusicDriver {
 	System.out.println(myMusic.toString());
 	Music anotherMusic = new Music("Back in Black", "AC/DC", "Metal", "CD");
 	System.out.println(anotherMusic.toString());
+	System.out.println(anotherMusic.getAlbum());
         }
   }
